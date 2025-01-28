7caf8f2058d9b07d9453abdba3be511dc311eb84
Fernando <fbsilva08@hotmail.com>
2025-01-27 22:36:59 -0300

    Commit inicial

---
+++ ms-event-manager/src/test/java/org/example/mseventmanager/MsEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mseventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
