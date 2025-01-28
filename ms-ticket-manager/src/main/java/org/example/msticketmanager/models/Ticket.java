f737402732f2e05c6f8e4682ec7c3b5fb7a36352
Fernando <fbsilva08@hotmail.com>
2025-01-27 23:59:09 -0300

    feat: adicionada conexão ao banco de dados e instanciados atributos do ticket

---
+++ ms-ticket-manager/src/main/java/org/example/msticketmanager/models/Ticket.java
@@ -0,0 +1,19 @@
+package org.example.msticketmanager.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Document(collection = "ticket")
+public class Ticket {
+    @Id
+    private String ticketId;
+    private String customerName;
+    private String cpf;
+    private String customerEmail;
+}
