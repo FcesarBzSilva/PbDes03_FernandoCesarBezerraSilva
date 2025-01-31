fd19c27fb0c0e9f146dae66106837f47a74d9376
Fernando <fbsilva08@hotmail.com>
2025-01-31 00:03:27 -0300

    test: Adiciona testes unitários para o método createTicket no TicketService

---
+++ ms-ticket-manager/src/test/java/org/example/msticketmanager/services/TicketServiceTest.java
@@ -0,0 +1,62 @@
+package org.example.msticketmanager.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.example.msticketmanager.clients.EventResourceClient;
+import org.example.msticketmanager.dto.EventDTO;
+import org.example.msticketmanager.dto.TicketDTO;
+import org.example.msticketmanager.infra.mqueue.TicketMqPublisher;
+import org.example.msticketmanager.models.Ticket;
+import org.example.msticketmanager.models.TicketDataMq;
+import org.example.msticketmanager.repository.TicketRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TicketServiceTest {
+
+    @Mock
+    private TicketRepository ticketRepository;
+
+    @Mock
+    private EventResourceClient eventResourceClient;
+
+    @Mock
+    private TicketMqPublisher ticketMqPublisher;
+
+    @InjectMocks
+    private TicketService ticketService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void createTicket_ShouldCreateTicket_WhenEventExists() throws JsonProcessingException {
+        Ticket ticket = new Ticket();
+        ticket.setEventId("event123");
+
+        EventDTO eventDTO = new EventDTO();
+        eventDTO.setId("event123");
+
+        when(eventResourceClient.getEventById("event123")).thenReturn(eventDTO);
+        when(ticketRepository.save(any(Ticket.class))).thenReturn(ticket);
+
+        TicketDTO result = ticketService.createTicket(ticket);
+
+        assertNotNull(result);
+        verify(ticketMqPublisher, times(1)).publishTicketNotification(any(TicketDataMq.class));
+        verify(ticketRepository, times(1)).save(ticket);
+    }
+}
+
+
